65972530f774447a149fe5040eba4f3839103ca8
AlFahimBinFaruk <alfahim.ps@gmail.com>
2025-05-06 15:47:52 +0600

    feat:swagger integrated

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get Server Status' })
+  @ApiResponse({ status: 200, description: 'Server is running!' })
   getHello(): string {
     return this.appService.getHello();
   }
