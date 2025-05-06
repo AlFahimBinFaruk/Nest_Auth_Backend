495f7fe5ee14c1fcb698ec7116bc6cd0315033f4
AlFahimBinFaruk <alfahim.ps@gmail.com>
2025-05-06 14:52:50 +0600

    feat:swagger added in auth.controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Body, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { ApiOperation,ApiCreatedResponse,ApiBadRequestResponse,ApiResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
@@ -9,12 +10,22 @@ export class AuthController {
         private authService:AuthService
     ){}
 
+
+    // Register new user.
     @Post('register')
+    @ApiOperation({ summary: 'Register a new user' })
+    @ApiCreatedResponse({ description: 'User registered successfully' })
+    @ApiBadRequestResponse({ description: 'Validation failed' })
     register(@Body() dto:RegisterDto){
         return this.authService.register(dto);
     }
 
+
+    // Login user
     @Post('login')
+    @ApiOperation({ summary: 'User login' })
+    @ApiResponse({ status: 200, description: 'Login successful' })
+    @ApiBadRequestResponse({ description: 'Invalid credentials or input' })
     login(@Body() dto:LoginDto){
         return this.authService.login(dto);
     }
